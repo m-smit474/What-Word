ec828628fecdd50fe8457e452d895af9980a294e
Matt Smith <smithmw99@gmail.com>
2023-02-12 11:09:14 -0700

    Refactor main in client

--- Client/Client.py
+++ Client/Client.py
@@ -4,7 +4,6 @@
 import socket
 import sys
 import PySimpleGUI as sg
-import time
 from Window import EXIT_BUTTON, window
 from Procedural import Procedural
 from InformationTheory import InformationTheory
@@ -53,7 +52,6 @@ def parseInput(rawInput):
             
     return output
 
-
 def getDifficulty():
     text = None
 
@@ -65,7 +63,6 @@ def getDifficulty():
 
     return text
 
-
 def displayNoServerMessage():
     sg.PopupError("No server found\n",
     "Please try again later or host the server.")
@@ -73,66 +70,125 @@ def displayNoServerMessage():
 def displayNoGameRunningMessage():
     sg.popup_no_buttons('Start a new game!')
 
+def updateWindow(command):
+    global running
+    global phrase
+    global guessedLetters
 
-    
+    # Send message to server
+    clientSocket.sendall(command.encode())
+
+    # Not guaranteed to read entire message
+    inFromServer = clientSocket.recv(1024).decode()
+    clientOutput = inFromServer #.lstrip('b')
+
+    update = clientOutput.split("@")[0]
+    lives = clientOutput.split("@")[1].split(" ")[0]
+    score = clientOutput.split("@")[1].split(" ")[1]
+    running = clientOutput.split("@")[1].split(" ")[2]
+    guessedLetters = clientOutput.split("@")[1].split(" ", 3)[3].replace("'","")
+
+    if guessedLetters == 'set()':
+        guessedLetters = ''
+
+    window["_DISPLAY_"].update(update)
+    window["-SCORE-"].update(score)
+    window["-LIVES-"].update(lives)
+    window["-GUESSED-"].update(guessedLetters)
+    window["-GUESS-"].update("")
+
+    phrase = update
+
+def procedural():
+    clicked = sg.popup_ok_cancel('Do you want to run the procedural algorithm?')
+
+    if clicked == 'OK':
+        runAlgorithm(Procedural())
+
+def informationTheory():
+    clicked = sg.popup_ok_cancel('Do you want to run the information theory algorithm?')
+
+    if clicked == 'OK':
+        runAlgorithm(InformationTheory())
+
+def runAlgorithm(algorithm):
+    while running == 'True':
+
+        event, values = window.read(2000)
+
+            # End program if user closes window
+        if event == "Exit" or event == EXIT_BUTTON:
+            break
+
+        guessLetter = algorithm.runAlgorithm(phrase, guessedLetters)
+        updateWindow('guess ' + guessLetter)
+
+def clientLoop():
+    global running
+    running = 'False'
+    isRestartable = False
+
+    # Client loop
+    while True:
+        event, values = window.read()
+
+        # End program if user closes window
+        if event == "Exit" or event == EXIT_BUTTON:
+            break
+        # Letters entered into guess bar
+        if event == '-GUESS-':
+            """
+            guess = values['-GUESS-']
+            update = guess
+            """
+        # Guess button clicked
+        if event == 'Guess!':
+            # ToDo error check here
+            if running != 'True':
+                displayNoGameRunningMessage()
+            else:
+                # Single character guesses must be a letter
+                if len(str(values['-GUESS-'])) == 1 and not str(values['-GUESS-'])[0].isalpha():
+                        sg.popup_no_buttons('Invalid Guess: ' + str(values['-GUESS-']),title="Invalid Guess")
+                else:
+                    updateWindow("guess " + str(values['-GUESS-']))
+        # New game button clicked
+        if event == 'NEW GAME':
+            dif = getDifficulty()
+
+            if dif:
+                isRestartable = True
+                updateWindow("start game " + str(dif))
+
+
+        # Restart button clicked
+        if event == 'Restart':
+            # You can only restart a game if one has already started
+            if isRestartable:
+                clicked = sg.popup_ok_cancel('Do you want to restart the game?')
+
+                if clicked == 'OK':
+                    updateWindow("restart")
+            else:
+                displayNoGameRunningMessage()
+
+        # Procedural button clicked
+        if event == 'Procedural':
+            if running != 'True':
+                displayNoGameRunningMessage()
+            else:
+                procedural()
+
+        # Information theory button clicked
+        if event == 'Info':
+            if running != 'True':
+                displayNoGameRunningMessage()
+            else:
+                informationTheory()
+
 
 def main():
-
-    def updateWindow(command):
-        global running
-        global phrase
-        global guessedLetters
-
-        # Send message to server
-        clientSocket.sendall(command.encode())
-
-        # Not guaranteed to read entire message
-        inFromServer = clientSocket.recv(1024).decode()
-        clientOutput = inFromServer #.lstrip('b')
-
-        update = clientOutput.split("@")[0]
-        lives = clientOutput.split("@")[1].split(" ")[0]
-        score = clientOutput.split("@")[1].split(" ")[1]
-        running = clientOutput.split("@")[1].split(" ")[2]
-        guessedLetters = clientOutput.split("@")[1].split(" ", 3)[3].replace("'","")
-
-        if guessedLetters == 'set()':
-            guessedLetters = ''
-
-        window["_DISPLAY_"].update(update)
-        window["-SCORE-"].update(score)
-        window["-LIVES-"].update(lives)
-        window["-GUESSED-"].update(guessedLetters)
-        window["-GUESS-"].update("")
-
-        phrase = update
-
-    def procedural():
-        clicked = sg.popup_ok_cancel('Do you want to run the procedural algorithm?')
-    
-        if clicked == 'OK':
-            runAlgorithm(Procedural())
-
-    def informationTheory():
-        clicked = sg.popup_ok_cancel('Do you want to run the information theory algorithm?')
-    
-        if clicked == 'OK':
-            runAlgorithm(InformationTheory())
-
-    def runAlgorithm(algorithm):
-        while running == 'True':
-
-            event, values = window.read(2000)
-
-             # End program if user closes window
-            if event == "Exit" or event == EXIT_BUTTON:
-                break
-
-            guessLetter = algorithm.runAlgorithm(phrase, guessedLetters)
-            updateWindow('guess ' + guessLetter)
-
-            
-
+    global clientSocket
 
     # Create a socket using IPv4 (AF_INET) and TCP (SOCK_STREAM)
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as clientSocket:
@@ -150,70 +206,7 @@ def main():
             sys.exit(1)
             
 
-        
-        
-
-        global running
-        running = 'False'
-        isRestartable = False
-
-        # Client loop
-        while True:
-            event, values = window.read()
-
-            # End program if user closes window
-            if event == "Exit" or event == EXIT_BUTTON:
-                break
-            # Letters entered into guess bar
-            if event == '-GUESS-':
-                """
-                guess = values['-GUESS-']
-                update = guess
-                """
-            # Guess button clicked
-            if event == 'Guess!':
-                # ToDo error check here
-                if running != 'True':
-                    displayNoGameRunningMessage()
-                else:
-                    # Single character guesses must be a letter
-                    if len(str(values['-GUESS-'])) == 1 and not str(values['-GUESS-'])[0].isalpha():
-                         sg.popup_no_buttons('Invalid Guess: ' + str(values['-GUESS-']),title="Invalid Guess")
-                    else:
-                        updateWindow("guess " + str(values['-GUESS-']))
-            # New game button clicked
-            if event == 'NEW GAME':
-                dif = getDifficulty()
-
-                if dif:
-                    isRestartable = True
-                    updateWindow("start game " + str(dif))
-
-
-            # Restart button clicked
-            if event == 'Restart':
-                # You can only restart a game if one has already started
-                if isRestartable:
-                    clicked = sg.popup_ok_cancel('Do you want to restart the game?')
-    
-                    if clicked == 'OK':
-                        updateWindow("restart")
-                else:
-                    displayNoGameRunningMessage()
-
-            # Procedural button clicked
-            if event == 'Procedural':
-                if running != 'True':
-                    displayNoGameRunningMessage()
-                else:
-                    procedural()
-
-            # Information theory button clicked
-            if event == 'Info':
-                if running != 'True':
-                    displayNoGameRunningMessage()
-                else:
-                    informationTheory()
+        clientLoop()
                 
             
         clientSocket.sendall(("exit").encode())
